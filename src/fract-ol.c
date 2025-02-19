211b282d28e450def1ac01ad8713309bf4acdd40
罗杰 <rmarrero@cbr9s4.42barcelona.com>
2025-02-19 17:06:25 +0100

    fract-ol.c [OK], norminette [KO]

--- src/fract-ol.c
+++ src/fract-ol.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fract-ol.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 17:05:01 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/02/19 17:06:02 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../include/fract-ol.h"
 
-int main(int argc, char **argv) {
-    t_fractal f;
+int	main(int argc, char **argv)
+{
+	t_fractal	f;
 
-    if (!parse_arguments(argc, argv, &f)) {
-        return (1);
-    }
-    init_fractal(&f);
-    render_fractal(&f);
-    mlx_key_hook(f.win, key_hook, &f);
-    mlx_hook(f.win, 4, 1L << 2, mouse_hook, &f);
-    mlx_hook(f.win, 17, 1L << 17, close_window, &f);
-    mlx_loop(f.mlx);
-    return (0);
+	if (!parse_arguments(argc, argv, &f))
+		return (1);
+	init_fractal(&f);
+	render_fractal(&f);
+	mlx_key_hook(f.win, key_hook, &f);
+	mlx_hook(f.win, 4, 1L << 2, mouse_hook, &f);
+	mlx_hook(f.win, 17, 1L << 17, close_window, &f);
+	mlx_loop(f.mlx);
+	return (0);
 }

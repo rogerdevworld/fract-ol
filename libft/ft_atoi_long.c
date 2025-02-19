b653d6c95a4d6712897321361f699df7cae4ed8c
罗杰 <rmarrero@cbr9s4.42barcelona.com>
2025-02-19 17:32:28 +0100

    libft [OK], norminette [KO]

--- libft/ft_atoi_long.c
+++ libft/ft_atoi_long.c
@@ -1,44 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atof.c                                          :+:      :+:    :+:   */
+/*   ft_atoi_long.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/19 15:39:15 by rmarrero          #+#    #+#             */
-/*   Updated: 2025/02/19 15:47:34 by rmarrero         ###   ########.fr       */
+/*   Created: 2025/01/24 16:08:38 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/01/24 16:09:52 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-double	ft_atof(const char *str)
+// --- atoi version for long numbers --- //
+long	ft_atoi_long(const char *nptr)
 {
-	double	result;
-	double	fraction;
+	long	result;
+	size_t	i;
 	int		sign;
 
-	result = 0.0;
-	fraction = 1.0;
 	sign = 1;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
-	if (*str == '-')
+	i = 0;
+	result = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-')
 	{
 		sign = -1;
-		str++;
+		i++;
 	}
-	else if (*str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
-		result = result * 10.0 + (*str++ - '0');
-	if (*str == '.')
+	else if (nptr[i] == '+')
+		i++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		str++;
-		while (*str >= '0' && *str <= '9')
-		{
-			fraction *= 0.1;
-			result += (*str++ - '0') * fraction;
-		}
+		result = result * 10 + (nptr[i] - '0');
+		i++;
 	}
 	return (result * sign);
 }

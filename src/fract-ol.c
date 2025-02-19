405c2f4d6da71e74e47f84eaed4f041cde10261c
罗杰 <rmarrero@cbr9s4.42barcelona.com>
2025-02-19 21:07:55 +0100

    fract-ol.c [OK], norminette [KO]

--- src/fract-ol.c
+++ src/fract-ol.c
@@ -11,6 +11,43 @@
 /* ************************************************************************** */
 #include "../include/fract-ol.h"
 
+int	key_hook(int keycode, t_fractal *f)
+{
+	if (keycode == 65307)
+	{
+		close_window(f);
+		exit(0);
+	}
+	else if (keycode == 49)
+		f->color_scheme = 1;
+	else if (keycode == 50)
+		f->color_scheme = 2;
+	else if (keycode == 51)
+		f->color_scheme = 3;
+	else if (keycode == 52)
+		f->color_scheme = 4;
+	render_fractal(f);
+	return (0);
+}
+
+int	mouse_hook(int button, int x, int y, t_fractal *f)
+{
+	if (button == 4)
+	{
+		f->zoom *= 1.1;
+		f->move_x += (x - f->width / 2.0) * 0.001 / f->zoom;
+		f->move_y += (y - f->height / 2.0) * 0.001 / f->zoom;
+	}
+	else if (button == 5)
+	{
+		f->zoom /= 1.1;
+		f->move_x += (x - f->width / 2.0) * 0.001 / f->zoom;
+		f->move_y += (y - f->height / 2.0) * 0.001 / f->zoom;
+	}
+	render_fractal(f);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_fractal	f;

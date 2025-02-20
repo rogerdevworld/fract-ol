06bea6dac2764a030556c971aff79da6450c99f9
罗杰 <rmarrero@cbr15s6.42barcelona.com>
2025-02-20 21:28:59 +0100

    julia.c [OK], norminette [OK]

--- src/fractals/julia.c
+++ src/fractals/julia.c
@@ -9,7 +9,7 @@
 /*   Updated: 2025/02/19 20:24:16 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../../include/fract-ol.h"
+#include "../../include/fractol.h"
 
 static void	process_julia_pixel(t_fractal *f, int x, int y)
 {

494eb3eb51476c4b7bf942d609e18110ca6788a2
罗杰 <rmarrero@cbr12s6.42barcelona.com>
2025-02-24 05:24:04 +0100

    parse.c [OK], norminette [OK]

--- src/parse.c
+++ src/parse.c
@@ -52,7 +52,7 @@ int	validate_arguments(int argc, char **argv)
 	char	*valid_fractals;
 
 	valid_fractals = "MJB";
-	if (argc < 1)
+	if (argc < 3)
 	{
 		show_usage();
 		return (0);
@@ -69,7 +69,8 @@ int	validate_arguments(int argc, char **argv)
 
 int	validate_julia_params(char **argv, t_fractal *f)
 {
-	if (!is_valid_number(argv[2]) || !is_valid_number(argv[3]))
+	if (!is_valid_number(argv[2]) || !is_valid_number(argv[3])
+		|| ft_isdigit(ft_atoi(argv[4])))
 	{
 		ft_printf("Error: Julia requiere c_real, ");
 		ft_printf("c_imag y max_iter como números válidos.\n");
@@ -78,12 +79,9 @@ int	validate_julia_params(char **argv, t_fractal *f)
 	}
 	f->c_julia_real = ft_atof(argv[2]);
 	f->c_julia_imag = ft_atof(argv[3]);
-	if (argv[4] && is_valid_integer(argv[4]))
-		f->max_iter = ft_atoi(argv[4]);
-	else
-		f->max_iter = 1;
-	if (f->c_julia_real < JULIA_REAL_MIN || f->c_julia_imag < JULIA_IMAG_MIN
-		|| f->c_julia_imag > JULIA_IMAG_MAX || f->c_julia_real > JULIA_REAL_MAX)
+	f->max_iter = ft_atoi(argv[4]);
+	if (f->c_julia_real < JULIA_REAL_MIN || f->c_julia_real > JULIA_REAL_MAX
+		|| f->c_julia_imag < JULIA_IMAG_MIN || f->c_julia_imag > JULIA_IMAG_MAX)
 	{
 		ft_printf("Error: Valores de Julia fuera de rango (%.1f a %.1f).\n",
 			JULIA_REAL_MIN, JULIA_REAL_MAX);
@@ -97,23 +95,19 @@ int	set_fractal_parameters(int argc, char **argv, t_fractal *f)
 	f->name = argv[1];
 	if (f->name[0] == 'J')
 	{
-		if (argc < 4 || argc > 5 || !validate_julia_params(argv, f))
-			// Cambié el check de argc
+		if (argc != 5 || !validate_julia_params(argv, f))
 			return (0);
 	}
 	else
 	{
-		if (argc != 2)
+		if (argc != 3 || !is_valid_integer(argv[2]))
 		{
 			ft_printf("Error: este fractal no requiere ");
 			ft_printf("parámetros adicionales o el max_iter no es válido.\n");
 			show_usage();
 			return (0);
 		}
-		if (argc == 3 && ft_atoi(argv[2]) && ft_atoi(argv[2]) > 1)
-			f->max_iter = ft_atoi(argv[2]);
-		else
-			f->max_iter = 1;
+		f->max_iter = ft_atoi(argv[2]);
 	}
 	if (f->max_iter < MAX_ITER_MIN || f->max_iter > MAX_ITER_MAX)
 	{
